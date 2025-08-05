314f448406a56f1294be8da5175509bd5428563b
DavidMateo88 <107157309+DavidMateo88@users.noreply.github.com>
2025-08-05 17:44:19 -0300

    Update script.js
    error url mal copiada

--- public/script.js
+++ public/script.js
@@ -13,7 +13,7 @@ async function login() {
   }
 
   try {
-    const response = await fetch('http://localhost:5000/api/depositos', {
+    const response = await fetch('http://localhost:5000/api/auth/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ username, password })
